717f2d3762375cf9ec46b62b4068dc3130ac330d
pratamaAkbae17 <akbarmulyapratama17@gmail.com>
2025-01-08 10:17:36 +0700

    Add files via upload

---
+++ variable.js
@@ -0,0 +1,45 @@
+let nama = "Akbar Mulya Pratama";
+console.log(nama);
+ 
+let alamat = "Demak";
+console.log(alamat);
+
+let usia = "15 tahun";
+console.log(usia);
+
+let ttl = "17 Maret 2009";
+console.log(ttl);
+
+let Gender = "Pria";
+console.log(Gender);
+
+let agama = "Islam";
+console.log(agama);
+
+let email = "akbarmulyapratama@gmail.com";
+console.log(email);
+
+let berat = "58 kg";
+console.log(berat);
+
+let tinggi = "168 cm";
+console.log(tinggi);
+
+let hobi = "Bermain bola dan gym";
+console.log(hobi);
+
+let cita = "pembisnis dan pemain sepak bola";
+console.log(cita);
+
+let student = "Pelajar";
+console.log(student);
+
+let sekolah = "SMKN 1 SAYUNG";
+console.log(sekolah);
+
+let jurusan = "PPLG";
+console.log(jurusan);
+
+
+
+document.write(" nama saya adalah "+ nama + " saya tinggal di "+ alamat + " pada tahun 2025 ini usia saya adalah "+ usia + " saya lahir di "+ttl +" jenis kelamin saya adalah "+ Gender + " saya beragama "+ agama + " alamat email saya "+ email + " saya memiliki berat badan "+ berat + " dengan tinggi "+ tinggi + " saya sangat senang bermain "+ hobi + "karena saya ingin memiliki cita-cita "+ cita + " saat ini saya adalah seorang "+ student + " yang bersekolah di "+ sekolah + " pada jurusan "+ jurusan)
