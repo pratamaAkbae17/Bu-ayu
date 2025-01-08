36a8907e17ecc7a34a6b98fec765a75fc8372872
pratamaAkbae17 <akbarmulyapratama17@gmail.com>
2025-01-08 10:21:34 +0700

    Add files via upload

--- variable.js
+++ variable.js
@@ -1,44 +1,30 @@
 let nama = "Akbar Mulya Pratama";
-console.log(nama);
  
 let alamat = "Demak";
-console.log(alamat);
 
 let usia = "15 tahun";
-console.log(usia);
 
 let ttl = "17 Maret 2009";
-console.log(ttl);
 
 let Gender = "Pria";
-console.log(Gender);
 
 let agama = "Islam";
-console.log(agama);
 
 let email = "akbarmulyapratama@gmail.com";
-console.log(email);
 
 let berat = "58 kg";
-console.log(berat);
 
 let tinggi = "168 cm";
-console.log(tinggi);
 
 let hobi = "Bermain bola dan gym";
-console.log(hobi);
 
 let cita = "pembisnis dan pemain sepak bola";
-console.log(cita);
 
 let student = "Pelajar";
-console.log(student);
 
 let sekolah = "SMKN 1 SAYUNG";
-console.log(sekolah);
 
 let jurusan = "PPLG";
-console.log(jurusan);
 
 
 
